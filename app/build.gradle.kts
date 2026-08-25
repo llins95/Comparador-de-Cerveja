61a203202bbc8fb9ef20a2fe3b42913d72f95a8c
llins95 <42194602+llins95@users.noreply.github.com>
2026-08-25 19:30:42 -0300

    chore: preparar versão 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.llins95.comparadordecerveja"
         minSdk = 23
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
