a53b1e3b2c2dabb852c4260204114ec6c208f140
llins95 <42194602+llins95@users.noreply.github.com>
2026-08-25 07:09:00 -0300

    feat: cria atividade principal

---
+++ app/src/main/java/com/llins95/comparadordecerveja/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.llins95.comparadordecerveja
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.llins95.comparadordecerveja.data.AppDatabase
+import com.llins95.comparadordecerveja.data.BeerRepository
+import com.llins95.comparadordecerveja.ui.BeerApp
+import com.llins95.comparadordecerveja.ui.BeerViewModel
+import com.llins95.comparadordecerveja.ui.theme.ComparadorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val database = AppDatabase.getInstance(applicationContext)
+        val repository = BeerRepository(database.beerOfferDao())
+
+        setContent {
+            ComparadorTheme {
+                val beerViewModel: BeerViewModel = viewModel(
+                    factory = BeerViewModel.factory(repository)
+                )
+                BeerApp(viewModel = beerViewModel)
+            }
+        }
+    }
+}
