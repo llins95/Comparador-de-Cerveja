0714b8bdb1c720c9871239b0486f97ff7676f18e
llins95 <42194602+llins95@users.noreply.github.com>
2026-08-28 12:41:27 -0300

    Preparar versão 0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.llins95.comparadordecerveja"
         minSdk = 23
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.5.1"
+        versionCode = 7
+        versionName = "0.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
