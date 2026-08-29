8fbc2a6403c747aca452766cf1af133fc42306b4
llins95 <42194602+llins95@users.noreply.github.com>
2026-08-29 15:54:41 -0300

    [release] Cerva 0.5.4 - editar promoção no simulador

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.llins95.comparadordecerveja"
         minSdk = 23
         targetSdk = 37
-        versionCode = 8
-        versionName = "0.5.3"
+        versionCode = 9
+        versionName = "0.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
